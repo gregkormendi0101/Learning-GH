f99e36456564e22fd9739b20ee9a7f344476cc09
Greg Kormendi <gregkormendi@gmail.com>
2022-12-26 18:19:56 -0500

    Initial Commit

---
+++ Learning GitHub/Learning_GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_GitHubApp.swift
+//  Learning GitHub
+//
+//  Created by Greg Kormendi on 12/26/22.
+//
+
+import SwiftUI
+
+@main
+struct Learning_GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
